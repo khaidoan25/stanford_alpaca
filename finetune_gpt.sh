bfc89463dfac2f9acaf701eaa8d3cc3a1813de20
Khai Doan <khaidoan25@gmail.com>
2023-03-28 15:14:54 -0400

    sh script to finetune model

---
+++ finetune_gpt.sh
@@ -0,0 +1,21 @@
+: "${MODEL_PATH?environment variable MODEL_PATH is unset}"
+
+torchrun --standalone --nproc_per_node=2 train.py \
+    --model_name_or_path $MODEL_PATH \
+    --data_path ./data/alpaca_5000.json \
+    --fp16 True \
+    --output_dir ./gpt_finetune \
+    --num_train_epochs 3 \
+    --per_device_train_batch_size 8 \
+    --per_device_eval_batch_size 8 \
+    --gradient_accumulation_steps 8 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 500 \
+    --save_total_limit 1 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+
